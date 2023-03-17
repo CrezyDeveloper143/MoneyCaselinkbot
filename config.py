793f004cf4accfa249e7b22ced43e67f43169e0d
CrezyDeveloper143 <117562112+CrezyDeveloper143@users.noreply.github.com>
2023-03-17 15:18:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,19 +10,19 @@ load_dotenv()
 API_ID = int(os.getenv("API_ID", "Your Api Id"))
 API_HASH = os.environ.get("API_HASH", "Your Api Hash")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "Bot Token")
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("Owner Id")] if os.environ.get("ADMINS") else []
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5989342270")] if os.environ.get("ADMINS") else []
 ADMIN = ADMINS
-DATABASE_NAME = os.environ.get("DATABASE_NAME", "Db Name")
-DATABASE_URL = os.getenv("DATABASE_URL", "Mongodb url") 
-OWNER_ID =  int(os.environ.get("OWNER_ID", "Owner Id")) 
-ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-ADMINS.append(1255023013)
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "urlshortnerbo")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://postbot:postbot@cluster0.ouwne8q.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5989342270")) 
+ADMINS.append(5989342270) if OWNER_ID not in ADMINS else []
+ADMINS.append(5989342270)
 
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "Logs Channels Id")) 
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001963546070")) 
 
 # For Force Subscription
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "Updates Channel User name Without @")
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "MoneyCaselinkupdate")
 
 # true if forward should be avoided
 BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True")
