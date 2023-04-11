bddcc81e23b6738898d18b71e25dc1fe070b5eb2
CrezyDeveloper143 <117562112+CrezyDeveloper143@users.noreply.github.com>
2023-04-11 22:05:50 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/CrezyDeveloper143/Shortner-Converter-Bot.git /Shortner-Converter-Bot
-cd /Shortner-Converter-Bot
+git clone https://github.com/CrezyDeveloper143/MoneyCaselinkbot.git /MoneyCaselinkbot
+cd /MoneyCaselinkbot
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
